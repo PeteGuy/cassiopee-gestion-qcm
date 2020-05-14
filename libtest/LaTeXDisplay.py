147df30751a7ae02b17d13de33cc2394c7a1f7c1
Guillem CANTO <guillem.canto@telecom-sudparis.eu>
2020-05-14 13:24:21 +0200

    Cropping pour ne voir que la question

--- libtest/LaTeXDisplay.py
+++ libtest/LaTeXDisplay.py
@@ -24,7 +24,7 @@ class Root():
         expr = self.entry.get("1.0",END) + "\\end{document}"
 
         the_color = "{" + self.master.cget('bg')[1:].upper() + "}"
-        preamble = r"\newcommand{\pathSrcStyleFiles}{/home/ollix/cassiopee-gestion-qcm/libtest/src_styles}"
+        preamble = r"\newcommand{\pathSrcStyleFiles}{/home/ollix/Travail/cassiopee-gestion-qcm/libtest/src_styles}"
         preamble += r"\documentclass{article}"
         preamble += r"\usepackage{pagecolor}"
         preamble += r"\usepackage[utf8x]{inputenc}"
@@ -47,7 +47,15 @@ class Root():
         img = PILImage.open(f)
         # See note at the bottom
         img.load(scale=5)
-        img = img.resize((int(img.size[0] / 2), int(img.size[1] / 2)), PILImage.BILINEAR)
+        img = img.resize((int(img.size[0] / 3), int(img.size[1] / 3)), PILImage.BILINEAR)
+
+        width, height = img.size
+        left = width/8
+        top = height/9
+        right = width
+        bottom = height/4
+        img = img.crop((left, top, right, bottom))
+
         photo = ImageTk.PhotoImage(img)
         self.label.config(image=photo)
         self.label.image = photo
